ab0920ce733e0fce32d0cbaaf0f976d8595f54e1
usuario <usuario@DESKTOP-SKS2JOK>
2026-04-09 13:58:27 +0200

    Segunda prueba

--- Proyecto1Git/src/paquete1/Principal.java
+++ Proyecto1Git/src/paquete1/Principal.java
@@ -3,7 +3,9 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println ("Estamos haciendo control de versiones con GIT");		// TODO Auto-generated method stub
+		System.out.println ("Estamos haciendo control de versiones con GIT");	
+		System.out.println ("Segunda prueba con un mensaje nuevo");
+		System.out.println ("Este es otro más");	// TODO Auto-generated method stub
 	}
 
 }
