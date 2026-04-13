87b8784981f5055d1b94677cc92872921ede1e6c
usuario <usuario@DESKTOP-SKS2JOK>
2026-04-13 11:19:00 +0200

    ULTIMO CAMBIO

--- Proyecto1Git/src/paquete1/Principal.java
+++ Proyecto1Git/src/paquete1/Principal.java
@@ -3,8 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println ("Estamos haciendo control de versiones con GIT");	
-		System.out.println ("Segunda prueba con un mensaje nuevo");
+		System.out.println ("Estamos haciendo control de versiones con GIT. ULTIMO CAMBIO REALIZADO");	
 	}
 
 }
