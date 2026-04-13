2d1c10983278e6a1908d286ae7a2b590b1cb82b2
usuario <usuario@DESKTOP-SKS2JOK>
2026-04-13 10:47:18 +0200

    otro

--- Proyecto1Git/src/paquete1/Principal.java
+++ Proyecto1Git/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println ("Estamos haciendo control de versiones con GIT");	
 		System.out.println ("Segunda prueba con un mensaje nuevo");
+		System.out.println ("Segunda prueba con un mensaje nuevo");
 	}
 
 }
